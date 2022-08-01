e6f189cc274c1fcd4f5ec2999ddfd25b71aaa9cc
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-08-01 21:13:41 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import Container from './Container/Container';
+import Profile from './Profile/index';
+import Statistics from './Statistics/index';
+import FriendList from './FriendList/index';
+import TransactionHistory from './TransactionHistory/index';
+import users from './Patch/user.json';
+import data from './Patch/data.json';
+import friends from './Patch/friends.json';
+import transactions from './Patch/transactions.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main>
+      <Container>
+        <Profile
+          username={users.username}
+          tag={users.tag}
+          location={users.location}
+          avatar={users.avatar}
+          followers={users.stats.followers}
+          views={users.stats.views}
+          likes={users.stats.likes}
+        />
+        <Statistics items={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+      </Container>
+    </main>
   );
 };
